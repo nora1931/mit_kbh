10e0ad85ed03610e6512f7d9b6114351be21d36c
Nora Benjnouh <norabenjnouh@MacBook-Pro-tilhrende-Nora.local>
2021-02-23 11:29:58 +0100

    pop up tilfoejet til master

--- script.js
+++ script.js
@@ -82,8 +82,8 @@ function popup_vindue(indhold) {
     document.querySelector(".pop_up").classList.remove("hide");
     document.querySelector("[popup_langbeskrivelse]").textContent = indhold.langtekst;
     document.querySelector("[popup_overskrift]").textContent = indhold.aktivitet;
-//    document.querySelector("[data-popupimg]").src = medieurl + indhold.billede;
-
+    document.querySelector("[popup_ikon]").src = medieurl + indhold.ikoner;
+    document.querySelector("[data-popupimg]").src = medieurl + indhold.billede;
 
 
 
